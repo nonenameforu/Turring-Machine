6151188c5972ddf8e75fd6756382a4659fcd3eab
__noname__ <139173318+nonenameforu@users.noreply.github.com>
2024-03-10 21:07:57 +0300

    Create ui_class.py

---
+++ ui_class.py
@@ -0,0 +1,27 @@
+
+import customtkinter as CTk
+
+class App(CTk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("960x540")
+        self.title("Turring machine")
+        
+    
+        
+        self.main_frame = CTk.CTkFrame(master=self , fg_color="black")
+        self.main_frame.pack(side = "top", fill= "both", expand = True)
+        
+        self.main_frame_bottom = CTk.CTkFrame(master = self)
+        self.main_frame_bottom.pack(side = "top", fill= "both", expand = True)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+app = App()
+app.mainloop()
