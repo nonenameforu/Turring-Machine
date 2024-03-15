12242b8bb45b92fce548a8d670d2e3d37c8a1101
__noname__ <139173318+nonenameforu@users.noreply.github.com>
2024-03-15 03:27:17 +0300

    big change in ui

--- study.py
+++ study.py
@@ -7,12 +7,17 @@ class ui():
         self.root = Tk()
         self.root.title("test_for_tkinter")
         self.root.geometry("1208x720")
-        self.root.resizable(False,False)
+        #self.root.resizable(False,False)
+        self.root.iconbitmap("resorse/free-icon-turing-1875989.ico")
         
-        img = PilImage.open(r"resorse/free-icon-turing-1875989.png")
-        img = img.resize((20,20),PilImage.ANTIALIAS)
-        self.image = ImageTk.PhotoImage(img)
+        self.entry = Entry(self.root)
+        #self.entry.grid(row=1,column=1)
         
+        
+        
+        # img = PilImage.open(r"resorse/free-icon-turing-1875989.png") открыетие 
+        # img = img.resize((20,20),PilImage.ADAPTIVE) и изменение  размера фото 
+        # self.image = ImageTk.PhotoImage(img) присовоение в переменную даной фотографии для того чтобы потом можно было ее использовать 
         
         ##self.label = Label(self.root, text="first_label",wraplength=20)
         ## лейбл для вывода фото  self.image = PhotoImage(file="resorse/free-icon-turing-1875989.png")
@@ -24,16 +29,33 @@ class ui():
         self.root.mainloop()
         
     def draw_widget(self):
-        Button(self.root, width=5 ,height=1 , text="text").grid(row=0, column=0)
         
+        for i in range(100):
+            text = " "+str(i)+" "
+            self.label = Label(self.root, text=text,bg="red",padx=10).grid(row=0, column=i,padx=5)
+            
+        for i in range(5):
+            self.entry.grid(row=1,column=i)
+        
+        
+        
+        #Button(self.root, text="444", command=self.button_action).grid(row=0, column=0)
+        
+         
+        #Button(self.root, image=self.image, command=self.button_action).grid(row=0, column=0) #создание и отрисовка кнопки 
         
         # self.label = Label(self.root, text="first_label",bg="red").grid(row=0, column=0)
         # self.label2 = Label(self.root , text="second_label", bg="green").grid(row=0, column=1)
         # self.Label3 = Label(self.root, text="third_label",bg="yellow").grid(row=1,column=0)
         # self.label4 = Label(self.root, text="fouth_label",bg="pink").grid(row=1, column=1)
     
+    def button_action(self):
+        text = self.entry.get()
+        self.label = Label(self.root, text=text,bg="red").grid(row=0, column=1)
+        print()
     
 if __name__ =="__main__":
     window = ui()
     window.run()
+    
     
--- ui_class.py
+++ ui_class.py
@@ -1,16 +1,118 @@
+import customtkinter as CTk
 
-import customtkinter 
 
-class App():
+class App(CTk.CTk):
     def __init__(self):
-        self.window = C
-        self.geometry("960x540")
+        super().__init__()
+        self.geometry("960x800")
         self.title("Turring machine")
         
-    def run(self):
-        self.window.mainloop()
+        ## Первый Фрейм ----------------------------------------------------------------------------------------------
+        self.Fist_frame = CTk.CTkFrame(master=self, fg_color="black").pack(side = "top", fill= "both", expand = True)
+        
+        
+        ## Второй Фрейм ------------------------------------------------------------------------------------------------
+        self.Second_frame = CTk.CTkFrame(master=self,height=50)
+        self.delete_last_state = CTk.CTkButton(master=self.Second_frame,height=10, width=50, text="Delete last state")
+        self.Add_states = CTk.CTkButton(master=self.Second_frame, height=10, width=50, text="Add state")
+        self.Start_machine = CTk.CTkButton(master=self.Second_frame, height=10, width=50, text="Start")
+        
+        self.delete_last_state.pack(side= "right",padx = 5)
+        self.Add_states.pack(side= "right",padx = 5)
+        self.Start_machine.pack(side= "left",padx = 5)
+        self.Second_frame.pack(side = "top", fill= "x")
+        
+        
+        ## Третий фрейм ----------------------------------------------------------------------------------------------
+        self.Third_frame = CTk.CTkTabview(master=self)
+        self.Third_frame.pack( side = "top", fill= "both", expand = True)
+        self.Third_frame.add("Q1")
+        self.Third_frame.add("Q2")
+        
+        
+        
+        # self.scrol = CTk.CTkFrame(self.Third_frame.tab("Q1"))
+        # self.ctk_textbox_scrollbar = CTk.CTkScrollbar(self.scrol,command=self.scrol,activate_scrollbars=False)
+        # self.scrol.configure(yscrollcommand=self.ctk_textbox_scrollbar.set)
+        # self.ctk_textbox_scrollbar.grid(row=0, column=1, sticky="ne")
+        #
+        # self.scrol_hor = CTk.CTkScrollableFrame(self.scrol,orientation="horizontal")
+        
+        # for i in range(30):
+        #     #for j in range (30):
+        #     enry = CTk.CTkEntry(self.scrol)
+        #     enry.grid(row = 0, column = i)
+        
+        # self.scrol.pack(fill= "both")
+        # self.scrol_hor.pack(fill= "both")
+
+        
+        # 
+        # 
+        
+        ## Четверый фрейм -------------------------------------------------------------------------------------------
+        self.Fourth_frame = CTk.CTkFrame(master=self,height=50)
+        
+        self.Add_comand = CTk.CTkButton(master=self.Fourth_frame,height=10, width=50, text="Add comand",command=self.Button_Add_comand)
+        self.Delete_comand = CTk.CTkButton(master=self.Fourth_frame,height=10, width=50, text="Delete comand")
+        
+        self.Add_tape = CTk.CTkButton(master=self.Fourth_frame,height=10, width=50, text="Add tape")
+        self.Delete_tape = CTk.CTkButton(master=self.Fourth_frame,height=10, width=50, text="Delete tape")
+        
+        self.Get_string = CTk.CTkEntry(master=self.Fourth_frame)
+        self.set_start = CTk.CTkEntry(master=self.Fourth_frame)
+        #self.set_start.configure(state = "0")
+        self.set_tape = CTk.CTkComboBox(master=self.Fourth_frame, state="readonly",values=["tape 1"])
+        self.Give = CTk.CTkButton(self.Fourth_frame,width=50, text="Give")
+        
+        self.Add_comand.pack(side = "left",padx = 5)
+        self.Delete_comand.pack(side = "left",padx = 5)
+        self.Get_string.pack(side = "left",padx = 15)
+        self.set_start.pack(side = "left",padx = 5)
+        self.set_tape.pack(side = "left",padx = 15)
+        self.Delete_tape.pack(side = "right",padx = 5)
+        self.Add_tape.pack(side = "right",padx = 5)
+        
+        self.Fourth_frame.pack(side = "bottom", fill= "x")
+
+    def Button_Add_comand (self):
+        self.ori = "horizontal"
+        
+        
+        
+        
+        
+        # self.main_frame = CTk.CTkFrame(master=self , fg_color="black")
+        # self.main_frame.pack(side = "top", fill= "both", expand = True)
+        
+        
+        # self.tab_control = CTk.CTknote
+        # self.main_frame_bottom = CTk.CTkFrame(master = self)
+        # self.main_frame_bottom.pack(side = "top", fill= "both", expand = True)
+        
+        # self.combo = CTk.CTkComboBox(self.main_frame,values=("1","2","3"),state="readonly")
+        # self.combo.grid(row = 1, column = 1)
+        
+        
+        
+        # self.Button = CTk.CTkButton(self.main_frame_bottom,text="gggg")
+        # self.Button.grid(row = 0 , column = 0)
+        
+        
         
 
-if __name__ == "__main__":
-    win = App()
-    win.run()
+        
+    
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+app = App()
+app.mainloop()
